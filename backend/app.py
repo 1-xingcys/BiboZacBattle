0cea31b2f7d0ab06bfa4cf703e60e3fa9e4b5e31
1xingcys <1xingcys@gmail.com>
2025-01-08 01:13:37 +0800

    [new feat] Manage event & round interface

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ from events.single_battle import create_single_battle, get_single_battle, end_si
                                     vote_single_battle, enter_single_battle
 from events.seven_to_smoke import create_seven_to_smoke
 from manage.player import generate_verification_code, save_to_file
+from events.rounds import get_rounds, get_players, create_single_round, start_round, stop_round
 
 
 FRONTEND_URL = "http://localhost:5173"
@@ -15,6 +16,63 @@ app = Flask(__name__)
 CORS(app)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('request_rounds')
+def handle_get_rounds(data):
+    event_id = data.get('eventId')
+    rounds = get_rounds(event_id)
+    if rounds is not None:
+        emit('response_round', rounds, broadcast=False)
+        
+@socketio.on('request_players')
+def handle_get_players(data):
+    event_id = data.get('eventId')
+    players = get_players(event_id)
+    if players is not None:
+        emit('response_players', players, broadcast=False)
+
+@socketio.on('request_create1on1')
+def handle_create_1on1(data):
+    # 提取前端傳遞的資料
+    info1on1 = data.get('info1on1')
+    event_id = data.get('eventId')
+
+    # 確認資料是否正確 
+    print(f"Received info1on1: {info1on1}")
+    print(f"Received eventId: {event_id}", flush=True)
+
+    # 獲取 info1on1 中的具體值
+    red_name = info1on1.get('red_name')
+    blue_name = info1on1.get('blue_name')
+    match_type = info1on1.get('type')
+    res = create_single_round(event_id, red_name, blue_name, match_type)
+    emit('response_create1on1', res, broadcast=False)
+    if res :
+        rounds = get_rounds(event_id)
+        if rounds is not None:
+            emit('response_round', rounds, broadcast=False)
+            
+@socketio.on('request_start_round')
+def handle_start_round(data):
+    event_id = data.get('eventId')
+    r_id = data.get('r_id')
+    res = start_round(event_id, r_id)
+    if res:
+        emit('response_start_round', res, broadcast=False)
+        rounds = get_rounds(event_id)
+        if rounds is not None:
+            emit('response_round', rounds, broadcast=False)
+
+@socketio.on('request_stop_round')
+def handle_stop_round(data):
+    event_id = data.get('eventId')
+    r_id = data.get('r_id')
+    res = stop_round(event_id, r_id)
+    if res:
+        emit('response_stop_round', res, broadcast=False)
+        rounds = get_rounds(event_id)
+        if rounds is not None:
+            emit('response_round', rounds, broadcast=False)
 
 @socketio.on('vote')
 def handle_vote(data):
@@ -73,5 +131,5 @@ def handle_sign_up():
     return jsonify({"name": name, "veri_code": unique_number})
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5002)
+    socketio.run(app, host='0.0.0.0', port=5000)
 
--- backend/models/db_utils.py
+++ backend/models/db_utils.py
@@ -11,6 +11,43 @@ def connect_to_database():
             conn = psycopg2.connect(DATABASE_URL)
             return conn
         except psycopg2.OperationalError:
-            print("Database not ready, retrying in 3 seconds...")
+            print("Database not ready, retrying in 3 seconds...", flush=True)
             time.sleep(3)
     raise Exception("Database connection failed after retries")
+
+# Execute an arbitrary query
+# 回傳 (是否成功, 受影響的資料筆數)
+def execute_query(query, data):
+    conn = connect_to_database()
+    cur = conn.cursor()
+    try:
+        cur.execute(query, data)
+        rowcount = cur.rowcount
+        conn.commit()
+        print(f"Query executed successfully {rowcount} rows are affected", flush=True)
+        return True, rowcount
+    except Exception as e:
+        conn.rollback()
+        print(f"Failed to execute query with data{data}: {e}", flush=True)
+        return False, 0
+    finally:
+        cur.close()
+        conn.close()
+        
+# Execute a SELECT query
+def execute_select_query(query, data=None):
+    conn = connect_to_database()
+    cur = conn.cursor()
+    try:
+        if data:
+            cur.execute(query, data)
+        else:
+            cur.execute(query)
+        results = cur.fetchall()
+        return results
+    except Exception as e:
+        print(f"Failed to execute select query: {e}", flush=True)
+        return []
+    finally:
+        cur.close()
+        conn.close()
